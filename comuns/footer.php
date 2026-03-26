90d7034a24545e968665efb9c8031d70b3819d10
joaobatu10 <cadorbatujoaovitor@gmail.com>
2026-03-25 23:20:53 -0300

    footer pronto

--- comuns/footer.php
+++ comuns/footer.php
@@ -1,2 +1,10 @@
+<footer>
+    <div class="container">
+        <div class="conteudo">
+            <p>Todos os direitos reservados</p>
+            <p>Desenvolvido por <a href="#"> Joao_cador <i class="fab fa-instagram"></i></a></p>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
